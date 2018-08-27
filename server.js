0e587b2f9c38379b589b8ccf372c09e9e815ba14
Dev Mani Shukla <devmanishukla@Devs-MacBook-Pro.local>
2018-08-27 20:19:59 +0530

    create projects page

--- server.js
+++ server.js
@@ -85,6 +85,13 @@ app.get('/about',(req,res) => {
     /* --------------------------------------*/
 });
 
+app.get('/projects',(req,res) => {
+    res.render('projects.hbs',{
+        pageTitle: 'Projects Page'
+    });
+    /* --------------------------------------*/
+});
+
 app.get('/bad',(req,res) => {
     //res.send('<h1>Hello Express !!!!!!!!</h1>');
     //res.send('Hello Express !!!!!!!!');
